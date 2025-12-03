55eafbf07a75f8d6da3a2e675111726a997d4c17
Paras Pradhan <paraspradhan123456789@gmail.com>
2025-12-04 01:46:29 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
-    int b = 20;
-    int c;
+    int a, b, c;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
     cout << "a = " << a << "\n";
     cout << "b = " << b << "\n";
-    //storing original value of a in temporary variable c
+    // storing original value of a in temporary variable c
     c = a;
-    //swapping value of b in a
+    // swapping value of b in a
     a = b;
-    //putting temporary variable i.e c's stored value in b
+    // putting temporary variable i.e c's stored value in b
     b = c;
     cout << "Afer Swapping" << "\n";
     cout << "a = " << a << "\n";
