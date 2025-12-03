3dce18da46a5c0e2700a39e1364461d8cc492f26
Paras Pradhan <paraspradhan123456789@gmail.com>
2025-12-03 23:05:01 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "----- Data Types and Their Sizes in C++ -----\n\n";
+
+    cout << "Size of char       : " << sizeof(char) << " byte(s)\n";
+    cout << "Size of short      : " << sizeof(short) << " byte(s)\n";
+    cout << "Size of int        : " << sizeof(int) << " byte(s)\n";
+    cout << "Size of long       : " << sizeof(long) << " byte(s)\n";
+    cout << "Size of long long  : " << sizeof(long long) << " byte(s)\n";
+    cout << "Size of float      : " << sizeof(float) << " byte(s)\n";
+    cout << "Size of double     : " << sizeof(double) << " byte(s)\n";
+    cout << "Size of long double: " << sizeof(long double) << " byte(s)\n";
+    cout << "Size of bool       : " << sizeof(bool) << " byte(s)\n";
+}
