3a48dffaada68aa8ca96949fe0bdffa189f244b7
Paras Pradhan <paraspradhan123456789@gmail.com>
2025-12-03 23:10:31 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     cout << "----- Data Types and Their Sizes in C++ -----\n\n";
-
+    //sizeof() prints the memory size of each C++ data type.
     cout << "Size of char       : " << sizeof(char) << " byte(s)\n";
     cout << "Size of short      : " << sizeof(short) << " byte(s)\n";
     cout << "Size of int        : " << sizeof(int) << " byte(s)\n";
@@ -14,4 +14,5 @@ int main()
     cout << "Size of double     : " << sizeof(double) << " byte(s)\n";
     cout << "Size of long double: " << sizeof(long double) << " byte(s)\n";
     cout << "Size of bool       : " << sizeof(bool) << " byte(s)\n";
+
 }
