1ac8285354794f55e79a0bf389e53c15d452b328
Paras Pradhan <paraspradhan123456789@gmail.com>
2025-12-04 01:04:47 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    cout << "Before Swapping: a = " << a << ", b = " << b << "\n";
+
+    // 1. a now holds the sum of the original a and b
+    a = a + b; // a = 10 + 20 = 30
+
+    // 2. b gets the original value of a (30 - 20 = 10)
+    b = a - b; // b = 30 - 20 = 10
+
+    // 3. a gets the original value of b (30 - 10 = 20)
+    a = a - b; // a = 30 - 10 = 20
+
+    cout << "After Swapping: a = " << a << ", b = " << b << "\n";
+
+    return 0;
+}
