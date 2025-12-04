0995f26690232c13d259038efafc67ccd4c4a382
Paras Pradhan <paraspradhan123456789@gmail.com>
2025-12-04 20:32:16 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double c, f;
+    cout << "Enter temprature in celsius units: ";
+    cin >> c;
+    f = c * 1.8 + 32;
+    cout << "Temprature in Fahrenheit= " << f;
+    return 0;
+}
