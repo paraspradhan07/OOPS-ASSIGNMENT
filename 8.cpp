1f3a72262b137e9e34a6d1ead281711fcd035660
Paras Pradhan <paraspradhan123456789@gmail.com>
2025-12-04 20:48:53 +0530

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    cout << "Enter a char value: ";
+    cin >> c;
+    int asciiValue = c;
+    cout << asciiValue;
+    return 0;
+}
